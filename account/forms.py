0377b1b3ad6cf5392b6c458d49ad3f4de986c806
alireza alavi <alavialireza38@gmail.com>
2022-03-17 13:37:20 +0330

    register form

--- account/forms.py
+++ account/forms.py
@@ -14,10 +14,50 @@ class LoginForm(forms.Form):
         label="رمز عبور"
     )
 
-    # def clean_user_name(self):
-    #     user_name = self.cleaned_data.get("user_name")
-    #     is_user_exists = User.objects.filter(username=user_name).exists()
-    #     if not is_user_exists:
-    #         raise forms.ValidationError("نام کاربری یا رمز عبور اشتباه است")
 
-    #     return user_name
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "لطفا نام کاربری خود را وارد کنید"}),
+        label="نام کاربری"
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"placeholder": "لطفا ایمیل خود را وارد کنید"}),
+        label="ایمیل",
+        validators=[
+            validators.EmailValidator("ایمیل وارد شده معتبر نمی باشد")
+        ])
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "لطفا رمز عبور خود را وارد کنید"}),
+        label="رمز عبور"
+    )
+
+    re_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "لطفا رمز عبور خود را تکرار کنید"}),
+        label="تکرار رمز عبور"
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        is_exists_by_username = User.objects.filter(username=username).exists()
+
+        if is_exists_by_username:
+            raise forms.ValidationError("نام کاربری زیر قبلا ثبت شده است.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        is_exists_by_email = User.objects.filter(email=email).exists()
+        if is_exists_by_email:
+            raise forms.ValidationError("ایمیل وارد شده قبلا ثبت شده است.")
+        return email
+
+    def clean_re_password(self):
+        password = self.cleaned_data.get("password")
+        re_password = self.cleaned_data.get("re_password")
+
+        if password != re_password:
+            raise forms.ValidationError("رمز های عبور مغایرت دارند")
+        return password
