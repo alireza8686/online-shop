83d390f7495a4cbaa6d396627f98705916fe08c4
alireza alavi <alavialireza38@gmail.com>
2022-03-25 11:55:16 +0430

    header

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from .views import login_view , register_view , log_out , user_panel
 urlpatterns = [
     path("login/", login_view,name='login'),
     path("register/", register_view,name='register'),
-    path("panel/", user_panel),
+    path("panel/", user_panel,name="panel"),
 #     path("panel/edit", panel_edit),
     path("log-out/", log_out,name='logout')
 ]
