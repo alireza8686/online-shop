51bf7e59cdcfdbca81990880243bd4ec14f3c44e
alireza alavi <alavialireza38@gmail.com>
2022-03-29 00:33:11 +0430

    orders

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Card,Order
 # Register your models here.
+
+
+
+admin.site.register(Card)
+admin.site.register(Order)
--- order/views.py
+++ order/views.py
@@ -12,18 +12,20 @@ def create_order(request):
 
     if order_form.is_valid():
         card = Card.objects.filter(user=request.user, is_paid=False).first()
-        if order is None:
-            order = Card.objects.create(user=request.user , is_paid=False)
-        product = order_form.cleaned_data.get("product")
+        if card is None:
+            card = Card.objects.create(user=request.user , is_paid=False)
+            card.save()
+        product_id = order_form.cleaned_data.get("product_id")
         count = order_form.cleaned_data.get("count")
         if count <= 0:
             count = 1
 
+        product = Product.objects.get(id=product_id)
         order = Order.objects.create(card=card,product=product,price = product.price , count = count)
         
         context = {
             'order' : order,
-            'card' : card
+            'card' : card,
         }
         return render(request,'order/orders.html',context)
     return render(request,'order/orders.html')
