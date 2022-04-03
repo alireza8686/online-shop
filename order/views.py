141df0a02d1efe82986313f509cdf8c571b1ad82
alireza alavi <alavialireza38@gmail.com>
2022-04-03 16:29:22 +0430

    orders

--- order/views.py
+++ order/views.py
@@ -1,4 +1,3 @@
-from itertools import count
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -17,6 +16,8 @@ def create_order(request,product_id):
     product = Product.objects.get(id=id)
     try:
         order = Order.objects.get(card=card,product=product,price = product.price)
+        order.count += 1
+        order.save()
     except:
         order = Order.objects.create(card=card,product=product,price = product.price,count=0)
         order.count += 1
@@ -29,13 +30,18 @@ def create_order(request,product_id):
 def order(request):
     card = Card.objects.get(user=request.user)
     orders = Order.objects.filter(card=card)
+    price = 0
+    for order in orders:
+        price += order.price * order.count
     context = {
-        'orders' : orders
+        'orders' : orders,
+        'price':price
     }
     return render(request,'order/card.html',context)
 
 
 def delete_order(request,id):
-    order = Order.objects.filter(id=id)
-    order.delete()
+    order = Order.objects.get(id=id)
+    order.count -= 1
+    order.save()
     return redirect('orders')
